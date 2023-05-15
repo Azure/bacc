396a5f832dbf9db4ac1450b96baa5440d9b18f3d
Utkarsh Ayachit <uayachit@microsoft.com>
2023-05-15 10:52:54 -0400

    more testing reorg

--- tests/cmake/secured_batch_tests.cmake
+++ tests/cmake/secured_batch_tests.cmake
@@ -6,6 +6,10 @@ configure_file("cmake/jumpbox_script.sh.in" "${SB_BINARY_DIR}/jumpbox_script.sh"
 
 sb_add_test(
     NAME "linux-jumpbox-dashboard"
-    COMMAND az vm run-command invoke -g ${SB_JUMPBOX_RESOURCE_GROUP} -n ${SB_JUMPBOX_NAME} --command-id RunShellScript --scripts @${SB_BINARY_DIR}/jumpbox_script.sh
+    COMMAND az vm run-command invoke
+        -g ${SB_JUMPBOX_RESOURCE_GROUP_NAME}
+        -n ${SB_JUMPBOX_NAME}
+        --command-id RunShellScript
+        --scripts @${SB_BINARY_DIR}/jumpbox_script.sh
     TIMEOUT 1800 # 30 minutes
 )
--- tests/cmake/validation_tests.cmake
+++ tests/cmake/validation_tests.cmake
@@ -4,9 +4,6 @@
 ## deployment is complete to ensure that the deployment
 ## was successful and has the expected resources.
 
-# include common utilities
-include(utils)
-
 # Confirm that the deployment is valid
 sb_add_test(
     NAME "validate-deployment"
@@ -14,14 +11,25 @@ sb_add_test(
     PASS_REGULAR_EXPRESSION "true"
 )
 
-# Confirm that requested pools have been created.
+# get list of all pools defined in the chosen configuration
 sb_get_pools(pool_names "${SB_CONFIG}")
 list(JOIN pool_names ".*" pool_names_regex)
+
+# Confirm that requested pools have been created.
 sb_add_test(
     NAME "validate-pools"
     COMMAND sb pool list -g ${SB_RESOURCE_GROUP_NAME} -s ${SB_SUBSCRIPTION_ID} --query "[].id" -o tsv
     PASS_REGULAR_EXPRESSION "${pool_names_regex}"
-    WILL_FAIL ${SB_TEST_SECURED_BATCH}
+    REQUIRES "SB_SUPPORTS_NETWORK_ACCESS"
+)
+
+# Confirm we don't have network access when not available
+sb_add_test(
+    NAME "validate-no-network-access"
+    COMMAND sb pool list -g ${SB_RESOURCE_GROUP_NAME} -s ${SB_SUBSCRIPTION_ID} --query "[].id" -o tsv
+    FAIL_REGULAR_EXPRESSION "AuthorizationFailure"
+    WILL_FAIL TRUE
+    REQUIRES "NOT SB_SUPPORTS_NETWORK_ACCESS"
 )
 
 # validate various pool properties.
@@ -31,7 +39,7 @@ foreach(pool_name IN LISTS pool_names)
         NAME "validate-pool-${pool_name}-id"
         COMMAND sb pool show -g ${SB_RESOURCE_GROUP_NAME} -s ${SB_SUBSCRIPTION_ID} -p ${pool_name} --query "id" -o tsv
         PASS_REGULAR_EXPRESSION "^${pool_name}"
-        WILL_FAIL ${SB_TEST_SECURED_BATCH}
+        REQUIRES "SB_SUPPORTS_NETWORK_ACCESS"
     )
 
     # verify pool vm size
@@ -40,7 +48,7 @@ foreach(pool_name IN LISTS pool_names)
         NAME "validate-pool-${pool_name}-vm-size"
         COMMAND sb pool show -g ${SB_RESOURCE_GROUP_NAME} -s ${SB_SUBSCRIPTION_ID} -p ${pool_name} --query "vmSize" -o tsv
         PASS_REGULAR_EXPRESSION "^${vm_size}"
-        WILL_FAIL ${SB_TEST_SECURED_BATCH}
+        REQUIRES "SB_SUPPORTS_NETWORK_ACCESS"
     )
 
     # verify pool has mounted volumes
@@ -49,7 +57,7 @@ foreach(pool_name IN LISTS pool_names)
         NAME "validate-pool-${pool_name}-mounts-count"
         COMMAND sb pool show -g ${SB_RESOURCE_GROUP_NAME} -s ${SB_SUBSCRIPTION_ID} -p ${pool_name} --query "mountConfiguration | length(@)" -o tsv
         PASS_REGULAR_EXPRESSION "^${mounts_count}"
-        WILL_FAIL ${SB_TEST_SECURED_BATCH}
+        REQUIRES "SB_SUPPORTS_NETWORK_ACCESS"
     )
 
     # verify pool is not using public IPs
@@ -57,6 +65,6 @@ foreach(pool_name IN LISTS pool_names)
         NAME "validate-pool-${pool_name}-no-public-ip"
         COMMAND sb pool show -g ${SB_RESOURCE_GROUP_NAME} -s ${SB_SUBSCRIPTION_ID} -p ${pool_name} --query "networkConfiguration.publicIpAddressConfiguration.provision" -o tsv
         PASS_REGULAR_EXPRESSION "^nopublicipaddresses"
-        WILL_FAIL ${SB_TEST_SECURED_BATCH}
+        REQUIRES "SB_SUPPORTS_NETWORK_ACCESS"
     )
 endforeach()
