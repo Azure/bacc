88975d9351f7010e457ee21427ee7f84d18f4f76
Utkarsh Ayachit <uayachit@microsoft.com>
2023-05-18 08:25:16 -0400

    testing cleanups

--- .github/ci/ctest.cmake
+++ .github/ci/ctest.cmake
@@ -5,11 +5,23 @@ macro(required_var var)
     endif()
 endmacro()
 
+macro(set_if_not_defined var value)
+    if (NOT DEFINED ${var})
+        set(${var} ${value})
+    endif()
+endmacro()
+
 required_var(SB_TEST_SUITE)
-required_var(SB_DEPLOYMENT_CONFIG)
+required_var(SB_CONFIG)
 required_var(SB_SUBSCRIPTION_ID)
 required_var(SB_RESOURCE_GROUP_NAME)
-required_var(SB_DEPLOYMENT_ENABLE_CONTAINERS)
+required_var(SB_SUPPORTS_ACR)
+required_var(SB_SUPPORTS_PACKAGES)
+
+set_if_not_defined(SB_TESTING_SKIP_POOL_DOWNSIZE OFF)
+set_if_not_defined(SB_SKIP_SUBMIT OFF)
+set_if_not_defined(SB_JUMPBOX_RESOURCE_GROUP_NAME "")
+set_if_not_defined(SB_JUMPBOX_NAME "")
 
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 ctest_start(Experimental)
@@ -29,12 +41,13 @@ endif()
 # Configure the project.
 set(cmake_args
     -DCTEST_USE_LAUNCHERS=1
-    -DSB_CONFIG:STRING=${SB_DEPLOYMENT_CONFIG}
+    -DSB_CONFIG:STRING=${SB_CONFIG}
     -DSB_SUBSCRIPTION_ID:STRING=${SB_SUBSCRIPTION_ID}
     -DSB_RESOURCE_GROUP_NAME:STRING=${SB_RESOURCE_GROUP_NAME}
     -DSB_JUMPBOX_RESOURCE_GROUP_NAME:STRING=${SB_JUMPBOX_RESOURCE_GROUP_NAME}
     -DSB_JUMPBOX_NAME:STRING=${SB_JUMPBOX_NAME}
-    -DSB_SUPPORTS_ACR:BOOL=${SB_DEPLOYMENT_ENABLE_CONTAINERS}
+    -DSB_SUPPORTS_ACR:BOOL=${SB_SUPPORTS_ACR}
+    -DSB_TESTING_SKIP_POOL_DOWNSIZE=${SB_TESTING_SKIP_POOL_DOWNSIZE}
     -C ${CMAKE_CURRENT_LIST_DIR}/configure-${SB_TEST_SUITE}.cmake
 )
 ctest_configure(OPTIONS "${cmake_args}")
@@ -43,7 +56,7 @@ ctest_configure(OPTIONS "${cmake_args}")
 ctest_test(RETURN_VALUE exit_code)
 
 # submit the results
-if (DEFINED SB_SKIP_SUBMIT AND "${SB_SKIP_SUBMIT}")
+if (SB_SKIP_SUBMIT)
     message(STATUS "Skipping test submission")
 else()
     # ctest_upload(
