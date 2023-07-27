9c7d49515c3a1c72703370c8ff779d7901e5798b
Utkarsh Ayachit <uayachit@microsoft.com>
2023-07-27 14:19:50 -0400

    await linux jumpbox provisioning

--- tests/cmake/secured_batch_tests.cmake
+++ tests/cmake/secured_batch_tests.cmake
@@ -5,6 +5,15 @@ include(utils)
 configure_file("cmake/jumpbox_script.sh.in" "${SB_BINARY_DIR}/jumpbox_script.sh" @ONLY)
 
 sb_add_test(
+    NAME "linux-jumpbox-await-provisioning"
+    COMMAND az vm wait
+        -g ${SB_JUMPBOX_RESOURCE_GROUP_NAME}
+        -n ${SB_JUMPBOX_NAME}
+        --created
+    TIMEOUT 1800 # 30 minutes
+)
+
+sb_add_test(
     NAME "linux-jumpbox-dashboard"
     COMMAND az vm run-command invoke
         -g ${SB_JUMPBOX_RESOURCE_GROUP_NAME}
@@ -13,4 +22,11 @@ sb_add_test(
         --scripts @${SB_BINARY_DIR}/jumpbox_script.sh
     PASS_REGULAR_EXPRESSION ".*100% tests passed,.*"
     TIMEOUT 1800 # 30 minutes
+)
+
+sb_test_workflow("linux-jumpbox-tests"
+    SETUP
+        "linux-jumpbox-await-provisioning"
+    TESTS
+        "linux-jumpbox-dashboard"
 )
